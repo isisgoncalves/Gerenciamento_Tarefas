96db2e1925f09da9f8aa6cf89b76ee8fc1983a03
isisgoncalves <isisvow@gmail.com>
2024-01-28 16:16:26 -0300

    Code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+  static void Main()
+  {
+    var titulo = "Atualizacao de Software";
+		var descricao = "Aplicar patches";
+		var dataVencimento = "22/12";
+        
+    titulo = Console.ReadLine();
+    descricao = Console.ReadLine();
+
+    if(descricao.Length > 50)
+    {
+      Console.WriteLine("Descricao ultrapassa limite de caracteres.");
+    }
+    else
+    {
+      dataVencimento = Console.ReadLine();
+      Console.WriteLine(descricao + " ate "+ dataVencimento);
+    }    
+  }
+}
